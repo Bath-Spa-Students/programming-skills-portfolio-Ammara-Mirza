acee1e2c0d2af3adc1ae1f76d0f1f3df755ebe93
Ammara-Mirza <144033297+Ammara-Mirza@users.noreply.github.com>
2023-10-12 09:19:28 +0400

    practice4.py

--- Chapter 1- Getting Started with Python/Exercises/practice4.py
+++ Chapter 1- Getting Started with Python/Exercises/practice4.py
@@ -2,4 +2,4 @@
 base=int(input('enter the base:'))
 height=int(input('enter th height:'))
 area=(base*height)/2
-print(('the area of the triangle is'),(area))
+print('the area of the triangle is',area)
