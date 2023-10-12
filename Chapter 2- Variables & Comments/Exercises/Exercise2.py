2c1e379663da748b41c04c480a7d3be2d5b037dc
Ammara-Mirza <144033297+Ammara-Mirza@users.noreply.github.com>
2023-10-12 13:49:07 +0400

    Exercise2.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise2.py
@@ -0,0 +1,10 @@
+'''Find a quote from a famous person you admire. Print the quote and the name of its author. 
+
+Your output should look something like the following, including the quotation marks:
+
+Albert Einstein once said, “A person who never made a mistake never tried anything new.'''
+
+str1="Prophet Muhammed pbuh once said:"
+str2=" “"
+str3="Seek knowledge from the cradle to the grave."
+print(str1+str2+str3)
