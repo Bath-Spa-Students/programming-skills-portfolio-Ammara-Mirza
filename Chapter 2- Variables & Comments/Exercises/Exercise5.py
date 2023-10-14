9c7a8cf8cd42ef5b9bfcf6beb87968ae2a877e0d
Ammara-Mirza <144033297+Ammara-Mirza@users.noreply.github.com>
2023-10-14 19:09:47 +0400

    exercise3.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise5.py
@@ -0,0 +1,15 @@
+'''A girl heads to a computer shop to buy some USB sticks. She loves USB sticks and wants as many as she can get for £50. They are £6 each.
+
+Write a programme that calculates how many USB sticks she can buy and how many pounds she will have left.
+
+You will to use the arithmetic operators to complete this exercise.'''
+
+x= 40   #Amount with the girl
+y= 2    #Amount for the USB stick
+z=x/y
+print("The girl can get" ,int(z),"USB sticks.")
+
+a = int(z)*y
+b = x-a
+print("the amount left with the girl is",b,"pounds.")
+
