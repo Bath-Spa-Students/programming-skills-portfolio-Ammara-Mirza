e108ef22a4faff32bcc93f4c58cebfef91102949
Ammara-Mirza <144033297+Ammara-Mirza@users.noreply.github.com>
2023-10-22 16:49:36 +0400

    exercise 3

--- Chapter 3- Data Structures/Exercises/Exercise3.py
+++ Chapter 3- Data Structures/Exercises/Exercise3.py
@@ -1,4 +1,4 @@
 ## Exercise 3: Your Own List :ballot_box_with_check: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
 name=["dodge challenger hellcat","dodge demon","ferrari viper","mercedes benz","ford raptor"]
-for i in names:
+for i in name:
     print("I want to own ",i )
