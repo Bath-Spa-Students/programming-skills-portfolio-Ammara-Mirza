110dd6a77270af231b744f9c583f477ff7ff3cbd
Ammara-Mirza <144033297+Ammara-Mirza@users.noreply.github.com>
2023-10-22 15:01:18 +0400

    Exercise 2

---
+++ Chapter 3- Data Structures/Exercises/Exercise2.py
@@ -0,0 +1,14 @@
+## Exercise 2: Greetings :ballot_box_with_check:Start with the list you used in Exercise 1, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+names= ['jack','olivia','bob','oggy']
+
+msg="hello, " + names[0].title() + "!"
+print (msg)
+
+msg="hello, " + names[1].title() + "!"
+print (msg)
+
+msg="hello, " + names[2].title() + "!"
+print (msg)
+
+msg="hello, " + names[3].title() + "!"
+print (msg)
