c17cda16d7f12d0100560ba5f861d66bb135edae
Ammara-Mirza <144033297+Ammara-Mirza@users.noreply.github.com>
2023-10-22 15:41:49 +0400

    Exercise 4

---
+++ Chapter 3- Data Structures/Exercises/Exercise4.py
@@ -0,0 +1,16 @@
+## Exercise 4: Guest List :ballot_box_with_check: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, invitingthem to dinner.
+guests=['jack','olivia','bob','oggy']
+
+name = guests[0].title()
+print(name+",please come to dinner.")
+
+name = guests[1].title()
+print(name+",please come to dinner.")
+
+name = guests[2].title()
+print(name+",please come to dinner.")
+
+name = guests[3].title()
+print(name+",please come to dinner.")
+
+
