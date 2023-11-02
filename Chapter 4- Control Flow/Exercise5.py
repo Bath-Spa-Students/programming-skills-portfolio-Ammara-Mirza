b3e67b89a65da94108212ce2ddb3d9557b629ed7
Ammara-Mirza <144033297+Ammara-Mirza@users.noreply.github.com>
2023-11-02 10:39:50 +0400

    Exercise5.py

---
+++ Chapter 4- Control Flow/Exercise5.py
@@ -0,0 +1,21 @@
+'''Exercise 5: Favorite Fruit :ballot_box_with_check:
+
+Make a list of your favorite fruits, and then write a series of independent if statements that check for certain fruits in your list.
+
+•Make a list of your three favorite fruits and call it favorite_fruits.
+
+•Write five if statements. Each should check whether a certain kind of fruit is in your list. If the fruit is in your list, the if block 
+
+should print a statement,such as You really like bananas!'''
+
+favorite_fruits=["apple","banana","melon"]
+if "grapes" in favorite_fruits:
+    print("you really like grapes")
+if "apple" in favorite_fruits:
+    print("you really like apple")
+if "kiwi" in favorite_fruits:
+    print("you really like kiwi")
+if "banana" in favorite_fruits:
+    print("you really like banana")
+if "melon" in favorite_fruits:
+    print("you really like melon")
