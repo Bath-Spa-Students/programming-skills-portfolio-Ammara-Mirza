d3da16b7326f01631e671a1653875c446fd8ada6
Ammara-Mirza <144033297+Ammara-Mirza@users.noreply.github.com>
2023-11-15 16:51:15 +0400

    exercise4.py

--- Chapter 5- Dictionaries/Exercise4.py
+++ Chapter 5- Dictionaries/Exercise4.py
@@ -6,20 +6,21 @@
 
 * Use a loop to print the name of each country included in the dictionary.'''
 
-rivers = {
-    'nile':'egypt',
+rivers ={
+    'nile':'egypt', 
     'mississippi':'united states',
     'faser':'canada',
-    'kuskokwim':'alaska'
+    'kuskokwim':'alaska',
     'yangtze':'china'
-}
+        }
+
 for river, country in rivers.items():
     print("The"+ river.title()+"flows through"+ country.title()+".")
 
 print("\nthe following rivers are included in this data set:")
-for river in rivers.key():
+for river in rivers.keys():
     print("-"+ river.title())
 
 print("\nthe following countries are included in this  data set:")
 for country in rivers.values():
-    print("-"+ country. title())
+    print("-"+ country.title())
